6e5c8a64d751a19117f7f8e95c961ca7a96a02ad
eddast <eddasr15@ru.is>
2018-07-12 23:07:33 +0000

    slider

--- src/ui/DateRange.tsx
+++ src/ui/DateRange.tsx
@@ -11,23 +11,23 @@ interface TimeRanges {
 }
 export default class DateRange extends React.Component <DateRangeProps, DateRangeState> {
   private PredefinedRanges: TimeRanges[] = [
-    {   name: 'Vika',         from: this.getDateWeeksBefore(1)      },
-    {   name: 'Tvær Vikur',   from: this.getDateWeeksBefore(2)      },
-    {   name: 'Mánuður',      from: this.getDateMonthsBefore(1)     },
-    {   name: 'Sex Mánuðir',  from: this.getDateMonthsBefore(6)     },
-    {   name: 'Ár',           from: this.getDateMonthsBefore(12)    },
-    {   name: 'Tvö ár',       from: this.getDateMonthsBefore(2*12)  },
-    {   name: 'Fimm ár',      from: this.getDateMonthsBefore(5*12)  },
+    {   name: 'vika',         from: this.getDateWeeksBefore(1)      },
+    {   name: 'tvær vikur',   from: this.getDateWeeksBefore(2)      },
+    {   name: 'mánuður',      from: this.getDateMonthsBefore(1)     },
+    {   name: 'sex mánuðir',  from: this.getDateMonthsBefore(6)     },
+    {   name: 'ár',           from: this.getDateMonthsBefore(12)    },
+    {   name: 'tvö ár',       from: this.getDateMonthsBefore(2*12)  },
+    {   name: 'fimm ár',      from: this.getDateMonthsBefore(5*12)  },
   ]
   getToday(): Date { return new Date() }
   getDateMonthsBefore(months: number): Date {
     let date = new Date();
-    date.setMonth(date.getMonth() - 6);
+    date.setMonth(date.getMonth() - months);
     return date;
   }
   getDateWeeksBefore(week: number): Date {
     let date = new Date();
-    date.setMonth(date.getDate() - 7);
+    date.setDate(date.getDate() - 7*week);
     return date;
   }
   componentWillMount(): void {
@@ -39,10 +39,24 @@ export default class DateRange extends React.Component <DateRangeProps, DateRang
     return (
       <div className='date-range-slider'>
         {this.PredefinedRanges.map((range: any, i: number) => {
-          const isActive: boolean = this.state.activeRange === range
+          console.log(range.name);
+          console.log(range.from);
+          const isActive: boolean = this.state.activeRange === range;
+          const lessThanActive: boolean = this.state.activeRange.from.getTime() < range.from.getTime();
           return([
-              <span key={i}><div className={isActive ? 'slide-indicator active' : 'slide-indicator'}/></span>,
-              <span key={i} className={isActive ? 'date-range-label active' : 'date-range-label'}>{range.name}</span>
+              <span onClick={() => this.setState({ activeRange: range })}>
+                <div className={
+                  isActive ? 'slide-indicator active' :
+                  lessThanActive ? 'slide-indicator less-than-active' :
+                  'slide-indicator'}
+                />
+              </span>,
+              <span
+                onClick={() => this.setState({ activeRange: range })}
+                className={isActive ? 'date-range-label active' : 'date-range-label'}
+              >
+                {range.name}
+              </span>
           ]);
         })}
       </div>
