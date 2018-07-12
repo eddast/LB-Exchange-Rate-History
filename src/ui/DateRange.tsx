27420d5487eeb5e4bf8f31809accae14f3bbad6a
eddast <eddasr15@ru.is>
2018-07-12 23:08:06 +0000

    started bar lines

--- src/ui/DateRange.tsx
+++ src/ui/DateRange.tsx
@@ -9,15 +9,15 @@ interface TimeRanges {
   name: string;
   from: Date;
 }
-export default class DateRange extends React.Component <DateRangeProps, DateRangeState> {
+export default class DateRange extends React.Component<DateRangeProps, DateRangeState> {
   private PredefinedRanges: TimeRanges[] = [
-    {   name: 'Vika',         from: this.getDateWeeksBefore(1)      },
-    {   name: 'Tvær Vikur',   from: this.getDateWeeksBefore(2)      },
-    {   name: 'Mánuður',      from: this.getDateMonthsBefore(1)     },
-    {   name: 'Sex Mánuðir',  from: this.getDateMonthsBefore(6)     },
-    {   name: 'Ár',           from: this.getDateMonthsBefore(12)    },
-    {   name: 'Tvö ár',       from: this.getDateMonthsBefore(2*12)  },
-    {   name: 'Fimm ár',      from: this.getDateMonthsBefore(5*12)  },
+    { name: 'Vika', from: this.getDateWeeksBefore(1) },
+    { name: 'Tvær Vikur', from: this.getDateWeeksBefore(2) },
+    { name: 'Mánuður', from: this.getDateMonthsBefore(1) },
+    { name: 'Sex Mánuðir', from: this.getDateMonthsBefore(6) },
+    { name: 'Ár', from: this.getDateMonthsBefore(12) },
+    { name: 'Tvö ár', from: this.getDateMonthsBefore(2 * 12) },
+    { name: 'Fimm ár', from: this.getDateMonthsBefore(5 * 12) },
   ]
   getToday(): Date { return new Date() }
   getDateMonthsBefore(months: number): Date {
@@ -35,14 +35,27 @@ export default class DateRange extends React.Component <DateRangeProps, DateRang
       activeRange: this.PredefinedRanges[3]
     });
   }
+
+  renderBar(i: number, isActive: boolean) {
+    if (i < this.PredefinedRanges.length - 1) {
+
+    } else if (i === 0) {
+
+    }
+    return <div className={isActive ? 'slide-bar active' : 'slide-bar'}></div>
+  }
+
   render(): JSX.Element {
     return (
       <div className='date-range-slider'>
         {this.PredefinedRanges.map((range: any, i: number) => {
           const isActive: boolean = this.state.activeRange === range
-          return([
-              <span key={i}><div className={isActive ? 'slide-indicator active' : 'slide-indicator'}/></span>,
-              <span key={i} className={isActive ? 'date-range-label active' : 'date-range-label'}>{range.name}</span>
+          return ([
+            <span key={i}>
+              <div className={isActive ? 'slide-indicator active' : 'slide-indicator'} />
+              {this.renderBar(i, isActive)}
+            </span>,
+            <span key={'label' + i} className={isActive ? 'date-range-label active' : 'date-range-label'}>{range.name}</span>
           ]);
         })}
       </div>
