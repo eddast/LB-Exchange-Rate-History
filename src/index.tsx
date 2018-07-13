7ec5116c7259e661dc51961884f7feb190bff1de
Edda Steinunn Rúnarsdóttir <erunarsdottir@ossur.com>
2018-07-13 10:33:51 +0000

    removed redux and store

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,6 @@
-import { Provider } from 'react-redux';
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
 import App from "./view/App";
-import { createStore, applyMiddleware } from 'redux';
-import reducers from './reducers/reducers';
-import thunk from 'redux-thunk';
 import './styles/mainTheme.css';
 
-const store = createStore(reducers, applyMiddleware(thunk));
-
-ReactDOM.render(
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/view/App.tsx
+++ src/view/App.tsx
@@ -7,18 +7,12 @@
 
 import * as React from 'react';
 import HistoricalExchangeRates from './HistoricalExchangeRates';
-import { Switch, Route, withRouter } from 'react-router-dom';
 
 interface AppState {}; interface AppProps {};
 export class App extends React.Component <AppProps, AppState> {
   public render() {
-    const paths = (
-        <Switch>
-            <Route exact path='/' component={HistoricalExchangeRates} />
-        </Switch>
-    );
-    return paths;
+    return <HistoricalExchangeRates/>;
   }
 }
 
-export default withRouter(App as any);
+export default App;
